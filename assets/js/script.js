a5a3a1ad434daa33c7c710ebf2bf5e47cfb81241
Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2020-09-24 16:59:26 +0600

    Updated some plagins

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+ jQuery(document).ready(function () {
+     
+     $('.counter').counterUp({
+         delay: 5,
+         time: 1000
+     });
+
+     $('.test_3').attr('data-date', dateText);
+
+//     $(window).on("load",function(){
+//         $('.preloader').fadeOut();
+//     });
+//     
+ });
